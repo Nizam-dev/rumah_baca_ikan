45b399865ebd7090864d22480efe21f8444c931a
anamoe <anam45188@gmail.com>
2022-11-23 12:46:01 +0700

    template-admin

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('user.beranda.beranda');
 });
+Route::get('admin',function(){
+    return view('admin.template.master');
+
+});
